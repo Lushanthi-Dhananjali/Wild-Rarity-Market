9617e20ccf9652b5a044dfd9e01a4e0f552d2756
Lushanthi-Dhananjali <dhananjalijaya@gmail.com>
2026-02-04 23:20:13 +0530

    Add product and list product

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
+import {Link} from 'react-router-dom'   
+import add_product_icon from '../../assets/add_product_icon.png'
+import list_product_icon from '../../assets/list_product_icon.png'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-
+        <Link to={'/addproduct'} style={{textDecoration: 'none'}}>
+        <div className="sidebar-item">
+            <img src={add_product_icon} alt="" className="" />
+            <p>Add Item</p>
+        </div>
+        </Link>
+        <Link to={'/listproduct'} style={{textDecoration: 'none'}}>
+        <div className="sidebar-item">
+            <img src={list_product_icon} alt="" className="" />
+            <p>List Items</p>
+        </div>
+        </Link>
     </div>
   )
 }
